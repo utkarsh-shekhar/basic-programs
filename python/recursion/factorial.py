8d474fdf077d37b547ca5264405cb0dca4975984
asdfMaciej <maciej.kaszkowiak@gmail.com>
2017-10-15 14:36:44 +0200

    Adding an example factorial program in Python 3.5

---
+++ python/recursion/factorial.py
@@ -0,0 +1,11 @@
+def factorial(n: int) -> int:  # Python >= 3.5
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+    # This can also be expressed as an one-liner:
+    # return 1 if !n else n * factorial(n-1)
+
+if __name__ == "__main__":  # executes only upon import
+    print("Factorial of 3 is: "+str(factorial(3)))
+    print("Factorial of 6 is: "+str(factorial(6)))
+    print("Factorial of 9 is: "+str(factorial(9)))
