57fbde27e8dfc3293e9e1b483fec34864f9187fc
Riya Pannu <riyapannu7@gmail.com>
2017-10-15 18:38:34 +0530

    Handling negative cases

--- C++/factorial.cpp
+++ C++/factorial.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 long long int factorial(long long int n) {
-    if (n == 0)
+    if (n<0)
+        return -1;
+    else if (n == 0)
         return 1;
     else
         return n * factorial(n-1);
@@ -13,10 +15,11 @@ long long int factorial(long long int n) {
 
 int main() {
     long long int n;
-    cout<<"Enter a number \n";
+    cout<<"Enter a positive number \n";
     cin >> n;
-    
     long long int result = factorial(n);
+    if (result==-1)
+        cout<<"Invalid input";
     cout << "The factorial of "<<n<<" is "<<result << endl;
     return 0;
 }
