8b71196140701a2d207bd3f1ccbdd059809a87c6
Moji TMJ <mojtabatmj@users.noreply.github.com>
2017-10-15 15:14:52 +0200

    Update factorial.py
    an alternative model for the negative number to put them equal to one.

--- python/factorial.py
+++ python/factorial.py
@@ -6,3 +6,13 @@ def factorial(n):
 
 # change the value for a different result
 print(factorial(5))
+
+##### an alternative model for the negative number to put them equal to one. because does not exist Factorial for negative numbers. ####
+
+n=int(input("Enter number:"))
+k=1
+while(n>0):
+    k=k*n
+    n=n-1
+print("Factorial of the number is: ")
+print(k)
