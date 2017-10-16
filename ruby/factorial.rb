bf849a8751c20e3be7bf89d34c4bd2427495c12a
Mukul Taneja <taneja.mukul@gmail.com>
2017-10-16 15:44:57 +0530

    Delete factorial.rb

--- ruby/factorial.rb
+++
@@ -1,9 +0,0 @@
-puts 'Hello there, Can you tell me the number you want factotial of?'
-number = gets.to_i
-fact = 1;
-
-(1..number).each do |i|
-    fact = fact*i;
-end
-
-print "Factorial of ", number ," is: ", fact
