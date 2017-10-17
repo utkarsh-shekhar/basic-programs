e3effceb34c7d4868a004b37a06f490d04ceb48a
vstelmashchuk <v.stelmashchuk@mobidev.biz>
2017-10-17 22:57:26 +0300

    Rename main file to Factorial

--- kotlin/src/Main.kt
+++
@@ -1,9 +0,0 @@
-import java.util.*
-
-fun main(arg: Array<String>) {
-    val scanner = Scanner(System.`in`)
-    val number = scanner.nextInt()
-    var factorial = 1
-    (2..number).forEach { factorial *= it }
-    System.out.println("Factorial of $number is: $factorial")
-}
