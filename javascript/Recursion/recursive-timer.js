1460f8f9c06befc222c230487eeec285371bbac3
CrowsVeldt <zechy.mahler@gmail.com>
2017-10-29 08:34:25 +0200

    Add recursive-timer to javascript/Recursion

---
+++ javascript/Recursion/recursive-timer.js
@@ -0,0 +1,23 @@
+// A countdown timer using recursion
+
+const time = new Date().getTime();
+
+const endTime = time + 10000;
+
+function startTimer (end) {
+  let timerID = setTimeout(() => {
+    
+    // These lines are simply for illustrative purposes
+    let sound = (timerID % 2 === 0) ? 'tick' : 'tock';
+    console.log(sound);
+    // 
+    
+    if (new Date().getTime() >= endTime) {
+      console.log('finished!');
+    } else {
+      startTimer(end);
+    }
+  }, 1000);
+}
+
+startTimer(endTime);
