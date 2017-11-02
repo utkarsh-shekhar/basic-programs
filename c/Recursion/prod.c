121fa150a2153e5dc915a14157764452d9d03e6d
luiscarlos.sf <luiscarlos.sf@outlook.com>
2017-11-02 16:03:53 -0300

    Review of comments. Inclusion of alert message for negative numbers.

--- c/Recursion/prod.c
+++ c/Recursion/prod.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /***
-	Function performs the product of numbers 0 and n.
+	Function prod performs the product of numbers 1 and n or zero.
+	Receives the last n (number) of the product.
+	Returns the product from 1 to n.
 ***/
 int prod (int n){
 	if(n==0){
@@ -11,9 +13,15 @@ int prod (int n){
 		return n*prod(n-1);
 	}
 }
+
+
 int main(){
 	int n;
 	scanf("%d", &n);
-	printf("Value: %d \nProduct: %d\n",n,prod(n));
+	if(n<0)
+		printf("The program only performs the output for positive numbers!\n");
+	else
+		printf("Value: %d \nProduct: %d\n",n,prod(n));
+
 	return 0;
 }
