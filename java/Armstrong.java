393722fc73c177724566a06ba9f18951627971d4
ridhikapila27 <72308639+ridhikapila27@users.noreply.github.com>
2020-10-10 20:03:08 +0530

    armstrong number program using do while loop

--- java/Armstrong.java
+++ java/Armstrong.java
@@ -3,14 +3,13 @@ class Armstrong{
     int c=0,a,temp;  
     int n=153;//It is the number to check armstrong  
     temp=n;  
-    while(n>0)  
-    {  
+    d0 {  
     a=n%10;  
+    c=c+(a*a*a); 
     n=n/10;  
-    c=c+(a*a*a);  
-    }  
+    } while(n>0);
     if(temp==c)  
-    System.out.println("armstrong number");   
+        System.out.println("armstrong number");   
     else  
         System.out.println("Not armstrong number");   
    }  
