4efdb84cd2f6d1e4e82b4cfdd98c4ba4499c35eb
Pratyushraj2121 <56498789+Pratyushraj2121@users.noreply.github.com>
2020-10-16 21:16:13 +0530

    Update insertion_sort.c

--- c/insertion_sort.c
+++ c/insertion_sort.c
@@ -1,32 +1,27 @@
 //INSERTION SORT with Time Complexity : n
 
 #include<stdio.h>
-int main()
-{
-   int i, j, r, temp, num[100000];
-   printf("Enter the interval: ");
-   scanf("%d",&r);
-   printf("Enter %d Values: \n", r);
-   for(i=0;i<r;i++)
-    {
-        scanf("%d",&num[i]);
-    }  
-    
-    for(i=1;i<r;i++)
-    {
-      temp=num[i];
-      j=i-1;
-      while((temp<num[j])&&(j>=0))
-        {
-            num[j+1]=num[j];
-            j=j-1;
-        }
-      num[j+1]=temp;
-   }
-   printf("Sorted Values: ");
-   for(i=0;i<r;i++)
-    {
-      printf("%d\t",num[i]);
-    }
-}
 
+int main() {
+  int i, j, r, temp, num[100000];
+  printf("Enter the interval: ");
+  scanf("%d", & r);
+  printf("Enter %d Values: \n", r);
+  for (i = 0; i < r; i++) {
+    scanf("%d", & num[i]);
+  }
+
+  for (i = 1; i < r; i++) {
+    temp = num[i];
+    j = i - 1;
+    while ((temp < num[j]) && (j >= 0)) {
+      num[j + 1] = num[j];
+      j = j - 1;
+    }
+    num[j + 1] = temp;
+  }
+  printf("Sorted Values: ");
+  for (i = 0; i < r; i++) {
+    printf("%d\t", num[i]);
+  }
+}
